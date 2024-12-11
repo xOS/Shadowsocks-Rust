8ca33f9008993e3031c99cdee521f2a8c3264a95
翠花 <admin@nange.cn>
2024-12-11 17:44:04 +0800

    Update Shadowsocks-Rust.sh

--- Shadowsocks-Rust.sh
+++ Shadowsocks-Rust.sh
@@ -9,7 +9,7 @@ export PATH
 #	WebSite: https://about.nange.cn
 #=================================================
 
-sh_ver="1.4.3"
+sh_ver="1.4.5"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file_1=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 FOLDER="/etc/ss-rust"
@@ -305,7 +305,7 @@ ${Green_font_prefix} 1.${Font_color_suffix} 开启  ${Green_font_prefix} 2.${Fon
 
 Set_password(){
 	echo "请输入 Shadowsocks Rust 密码 [0-9][a-z][A-Z]"
-	read -e -p "(默认：随机生成)：" password
+	read -e -p "(默认：随机生成 Base64)：" password
 	[[ -z "${password}" ]] && password=$(openssl rand -base64 16)
 	echo && echo "=================================="
 	echo -e "密码：${Red_background_prefix} ${password} ${Font_color_suffix}"
@@ -570,7 +570,14 @@ View(){
 	echo -e "——————————————————————————————————"
 	[[ ! -z "${link_ipv4}" ]] && echo -e "${link_ipv4}"
 	[[ ! -z "${link_ipv6}" ]] && echo -e "${link_ipv6}"
-	echo -e "——————————————————————————————————"
+	echo -e "—————————————————————————"
+	echo -e "${Info} Surge 配置："
+	if [[ "${ipv4}" != "IPv4_Error" ]]; then
+	echo -e "$(uname -n) = ss,${ipv4},${port},encrypt-method=${cipher},password=${password},tfo=${tfo},udp-relay=true,ecn=true"
+	else
+	echo -e "$(uname -n) = ss,${ipv6},${port},encrypt-method=${cipher},password=${password},tfo=${tfo},udp-relay=true,ecn=true"
+	fi
+  echo -e "—————————————————————————"
 	Before_Start_Menu
 }
 
